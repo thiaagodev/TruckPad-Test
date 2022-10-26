153123024a931bd22680d5538279ac3335c9506b
Thiago Ferreira <thiaago.dev@gmail.com>
2022-10-26 13:02:52 -0300

    main activity adjustments

--- app/src/main/java/com/thiaagodev/truckpad/MainActivity.kt
+++ app/src/main/java/com/thiaagodev/truckpad/MainActivity.kt
@@ -2,10 +2,20 @@ package com.thiaagodev.truckpad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thiaagodev.truckpad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
