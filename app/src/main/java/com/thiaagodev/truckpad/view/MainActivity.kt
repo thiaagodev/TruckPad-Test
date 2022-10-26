09c12b34c9ab296546f641e18535d65d36728a5a
Thiago Ferreira <thiaago.dev@gmail.com>
2022-10-26 15:21:05 -0300

    navigate activitys and view package

---
+++ app/src/main/java/com/thiaagodev/truckpad/view/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.thiaagodev.truckpad.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.thiaagodev.truckpad.R
+import com.thiaagodev.truckpad.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.buttonCalcNewShipping.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_calc_new_shipping -> openShippingForm()
+        }
+    }
+
+    private fun openShippingForm() {
+        startActivity(
+            Intent(this, ShippingCalcForm::class.java)
+        )
+
+    }
+}
---
+++ app/src/main/java/com/thiaagodev/truckpad/view/filter/MinMaxFilter.kt
@@ -0,0 +1,37 @@
+package com.thiaagodev.truckpad.view.filter
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class MinMaxFilter() : InputFilter {
+    private var intMin: Int = 0
+    private var intMax: Int = 0
+
+    constructor(minValue: Int, maxValue: Int) : this() {
+        this.intMin = minValue
+        this.intMax = maxValue
+    }
+
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dStart: Int,
+        dEnd: Int
+    ): CharSequence? {
+        try {
+            val input = Integer.parseInt(dest.toString() + source.toString())
+            if (isInRange(intMin, intMax, input)) {
+                return null
+            }
+        } catch (e: NumberFormatException) {
+            e.printStackTrace()
+        }
+        return ""
+    }
+
+    private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+        return if (b > a) c in a..b else c in b..a
+    }
+}
