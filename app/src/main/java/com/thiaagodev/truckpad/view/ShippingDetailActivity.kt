8384c320a5f024ab3f2196a5180d27a12045bb83
Thiago Ferreira <thiaago.dev@gmail.com>
2022-10-31 20:05:30 -0300

    shipping detail activity and initial layout

---
+++ app/src/main/java/com/thiaagodev/truckpad/view/ShippingDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.thiaagodev.truckpad.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thiaagodev.truckpad.R
+
+class ShippingDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shipping_detail)
+    }
+}
